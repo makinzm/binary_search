e4f56189af46214f025a6d9d017a4fd8a09cc298
nozomi maki <yuruseba@yahoo.co.jp>
2018-06-21 17:03:58 +0900

    jojo

--- src/spear.c
+++ src/spear.c
@@ -5,24 +5,31 @@ int k;
 int A[100000];
 
 int p(int m){
-  int i;
-  int l = 0;
-  for(i = 0; i < n; i++){
-    l = l + A[i]/m;
-  }
-  return 
-  }
-
-}
-
+   int i;
+   int l = 0;
+   for(i=0; i < n; i++){
+       l = l + A[i]/m;
+   }
+   return l < k;
+   }
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+   int i, lb, ub;
+   scanf("%d%d", &n, &k);
+   for(i = 0; i < n; i++){
+       scanf("%d", &A[i]);
+   }
+   lb = 0;
+   ub = 1000000000;
+   while(ub-lb > 1){
+       int m = (lb + ub)/2;
+       if(p(m)){
+           ub = m;
+       }
+       else{
+           lb = m;
+       }
+   }
+   printf("%d\n", lb);
+   return 0;
 }
