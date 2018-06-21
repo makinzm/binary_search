1229699e3492a136b736dfaf6b9c4a63379ace43
nozomi maki <yuruseba@yahoo.co.jp>
2018-06-21 16:34:24 +0900

    トトロ

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int i;
+  int l = 0;
+  for(i = 0; i < n; i++){
+    l = l + 1 + (A[i]-1)/m;
+  }
+  return k >= l;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int m = (lb + ub) / 2;
+    if (p(m)){
+      ub = m;
+    }
+    else {
+      lb = m;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,17 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+  int i;
+  int l = 0;
+  for(i = 0; i < n; i++){
+    l = l + A[i]/m;
+  }
+  return 
+  }
+
+}
+
 
 int main(){
   int i, lb, ub;
